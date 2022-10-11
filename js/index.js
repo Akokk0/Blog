f0bb8cc6e5e18dc8d27a445416b7e33a9b702347
akokko <zhminecraft@sina.com>
2022-10-11 23:23:29 +0800

    building blog-items

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-let bulc = document.getElementsByClassName('blog-user-list-content')[0].children
+/*let itemArray = document.getElementsByClassName('blog-user-list-content')[0].children
 // console.log(bulc)
-for (item of bulc) {
+for (item of itemArray) {
     console.log(item)
-    item.onclick = function () {
-        item.className = 'blog-user-list-item blog-user-list-item-active'
-    }
 }
+*/
+
+// 入口函数
+$(function () {
+    $('.blog-user-list-item').on('click', this, function () {
+        if ($(this).hasClass("blog-user-list-item-active")) {
+            // $(this).removeClass("blog-user-list-item-active");
+        } else {
+            $(".blog-user-list-item").each(function () {
+                $(this).removeClass("blog-user-list-item-active");
+            })
+            $(this).addClass("blog-user-list-item-active");
+        }
+    })
+})
+
+
